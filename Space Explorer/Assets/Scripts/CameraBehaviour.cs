85a305b713b99d89e0e59d453effb002aa8ad43c
bvalic <borna.valic@hotmail.com>
2018-01-09 14:09:40 +0100

    Movement&Camera
    Tin nemoj da se ljutis odma, tvoje skripte nisam diral. Napravil sam nove skripte za movement i kameru jer mi je vitoperenje kamere dizalo tlak. Ljepo molim da testiras ovu verziju, kod je napravljen za po 2 verzije kamere i kontrola pa provjeri i to. Ak ti se ne svidja koristit cemo tvoje skripte. BTW, naso sam gde ti je problem sa stabilizacijom broda tak da je i to sredjeno.

---
+++ Space Explorer/Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour {
+
+	public GameObject target;
+
+	Vector3 offset;
+
+	void Start () {
+		
+		offset = transform.position - target.transform.position;
+
+	}
+
+	void FixedUpdate () {
+
+		//oba 2 nacina rade
+		//kamera zaostaje za spaceshipom ovisno o brzini, al rotacija ga prati uzastopno, teze je vidit kam ides kod skretanja
+		//ako se kretanje ne radi preko rigidbodya kamera ne zaostaje
+		//transform.rotation = target.transform.rotation;
+		//Vector3 offsetTemp = offset + offset * (0.005f * target.GetComponent<Rigidbody> ().velocity.magnitude);
+		//transform.position = target.transform.position + (target.transform.rotation * offsetTemp);
+
+
+		//ovaj dio radi super
+		transform.rotation = Quaternion.Lerp(transform.rotation, target.transform.rotation, 15f * Time.deltaTime);
+		Vector3 tarPos = target.transform.position + (transform.rotation * offset);
+		Vector3 curPos = Vector3.Lerp(transform.position, tarPos, 10f * Time.deltaTime);
+		transform.position = curPos;
+
+	}
+}
---
+++ Space Explorer/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float moveSpeed = 10f;
+	public float turnSpeed = 5f;
+
+	Rigidbody rb;
+
+	float verticalRotation;
+	float horizontalRotation;
+
+	void Start () {
+		
+		rb = GetComponent<Rigidbody> ();
+
+	}
+
+	void FixedUpdate () {
+
+		//kretanje broda
+		float fwd = Input.GetAxis ("Vertical");
+		//rade oba 2 nacina, preko rigidbodya se zbog sile lijepi za asteroide, al ima prirodnije kretanje (ubrzavanje, usporavanje, drift)
+		//translate radi identicno ko na tvojoj skripti
+		rb.AddForce (fwd * transform.forward * moveSpeed, ForceMode.Impulse);
+		//transform.Translate(0, 0, fwd * moveSpeed * 5 * Time.deltaTime);
+
+
+		//rotacija broda
+		float x = Input.GetAxis ("Mouse X");
+		float y = Input.GetAxis ("Mouse Y");
+		horizontalRotation = x * turnSpeed;
+		verticalRotation = y * turnSpeed;
+
+		//limitacija brzine rotiranja, bez limita vrti se kolko brzo pomices mis, s limitima ima odredjenu maksmimalnu brzinu rotacije
+		verticalRotation = Mathf.Clamp (verticalRotation, -5f, 5f);
+		horizontalRotation = Mathf.Clamp (horizontalRotation, -5f, 5f);
+
+		transform.Rotate (-verticalRotation, 0f, 0f, Space.Self);
+		transform.Rotate (0f, horizontalRotation, 0f, Space.Self);
+	}
+}
